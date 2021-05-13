59ebd16427c047beb40a5d291b16b2d347e1c84e
Joseph K J <josephkj@google.com>
2021-05-13 10:20:35 +0530

    SVM:updates

--- svm.py
+++ svm.py
@@ -7,17 +7,16 @@ from sklearn.metrics import accuracy_score
 train_path = '/home/josephkj_google_com/workspace/disentangling-vae/neo_results/cifar_10_vae/training_features/'
 test_path = '/home/josephkj_google_com/workspace/disentangling-vae/neo_results/cifar_10_vae/testing_features/'
 
+train_path = '/home/joseph/workspace/disentangling-vae/neo_results/btcvae_cifar10/training_features/'
+test_path = '/home/joseph/workspace/disentangling-vae/neo_results/btcvae_cifar10/testing_features/'
+
 X = []
 y = []
 for file in os.listdir(train_path):
-    label = file.split('_')[1]
-    z = torch.load(os.path.join(train_path, file)).detach().numpy()
+    label = file.split('_')[1].split('.')[0]
+    z = torch.load(os.path.join(train_path, file)).detach().numpy()[0]
     X.append(z)
     y.append(label)
-
-# print(X)
-
-print(X.shape)
 
 print('Loaded the data.')
 
@@ -29,8 +28,8 @@ print('Fit the data.')
 X_test = []
 y_test = []
 for file in os.listdir(test_path):
-    label = file.split('_')[1]
-    z = torch.load(os.path.join(test_path, file)).detach().numpy()
+    label = file.split('_')[1].split('.')[0]
+    z = torch.load(os.path.join(test_path, file)).detach().numpy()[0]
     X_test.append(z)
     y_test.append(label)
 
